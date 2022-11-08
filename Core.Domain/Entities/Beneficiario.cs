b5088b945858f218464af1e14d8e79736fa7c94d
l9dson-wq <andelsonnicolas@gmail.com>
2022-11-09 00:13:36 +0100

    IdBeneficiario added to Beneficiario table

--- Core.Domain/Entities/Beneficiario.cs
+++ Core.Domain/Entities/Beneficiario.cs
@@ -8,6 +8,8 @@ public class Beneficiario
     
     public int IdUser { get; set; }
 
+    public int IdBeneficiario { get; set; }
+
     //Navigation property
     public User user { get; set; }
     public CuentaAhorro CuentaAhorro { get; set; }
