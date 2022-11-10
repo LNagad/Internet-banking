c5b83a296a833fbbba54ecf69206f62ab5e568e1
LNagad <nagadgames100k@gmail.com>
2022-11-10 12:05:11 -0400

    authentication services and dtos created, pending for email service to finish the registration

---
+++ Core.Application/Dtos/Account/RegisterRequest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Dtos.Account
+{
+    public class RegisterRequest
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Infrastructure.Identity/Services/AccountService.cs
@@ -0,0 +1,132 @@
+﻿using Core.Application.Dtos.Account;
+using Core.Application.Enums;
+using Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity.Services
+{
+    public class AccountService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager= userManager;
+            _signInManager= signInManager;
+        }
+
+        public async Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request)
+        {
+            AuthenticationResponse response = new();
+
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                response.HasError= true;
+                response.Error = $"No se encontro ninguna cuenta registrada con el correo {request.Email}";
+                return response;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, false, false);
+
+            if(!result.Succeeded)
+            {
+                response.HasError = true;
+                response.Error = $"Credenciales invalidas para el correo {request.Email}";
+                return response;
+            }
+
+            if(!user.EmailConfirmed)
+            {
+                response.HasError = true;
+                response.Error = $"{request.Email} no ha sido confirmada/o activada ";
+                return response;
+            }
+
+            response.Id = user.Id;
+            response.Email= user.Email;
+            response.UserName= user.UserName;
+
+            var rolesList = await _userManager.GetRolesAsync(user).ConfigureAwait(false);
+
+            response.Roles = rolesList.ToList();
+            response.IsVerified = user.EmailConfirmed;
+            
+            return response;
+        }
+
+
+        public async Task<RegisterResponse> RegisterBasicUserAsync(RegisterRequest request, string origin)
+        {
+            RegisterResponse response = new();
+            response.HasError = false;
+
+            var userWithSameUserName = await _userManager.FindByNameAsync(request.UserName);
+
+            if (userWithSameUserName != null)
+            {
+                response.HasError = true;
+                response.Error = $"UserName {request.UserName} ya esta registrado en el sistema";
+                return response;
+            }
+
+            var userWithSameEmail = await _userManager.FindByEmailAsync(request.Email);
+
+            if (userWithSameEmail != null)
+            {
+                response.HasError = true;
+                response.Error = $"Eail {request.Email} ya esta registrado en el sistema";
+                return response;
+            }
+
+            var user = new ApplicationUser
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                UserName = request.UserName,
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, Roles.Basic.ToString());
+
+                var verificationUri = await SendVerificationEmailUrl(user, origin);
+
+                //send email here
+            } 
+            else
+            {
+                response.HasError = true;
+                response.Error = $"Un error ocurrio tratando de crear el usuario.";
+                return response;
+            }
+
+
+            return response;
+        }
+
+
+        private async Task<string> SendVerificationEmailUrl(ApplicationUser user, string origin)
+        {
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var route = "User/ConfirmEmail";
+            var Uri = new Uri(string.Concat($"{origin}/", route));
+            var verificationUri = QueryHelpers.AddQueryString(Uri.ToString(), "userId", user.Id);
+            verificationUri = QueryHelpers.AddQueryString(Uri.ToString(), "token", code);
+
+            return verificationUri;
+        }
+    }
+}
